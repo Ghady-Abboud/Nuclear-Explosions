4de14935b09489ed5c7f5734d8428ec0c4c1e556
Ghady Abboud <abboudghady@gmail.com>
2024-11-14 18:05:15 -0500

    Fix Config Import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { Config } from '../config/environment.config';
+import { Config } from './config/environment.config';
 import routes from '../routes/routes';
 
 const app = express();
