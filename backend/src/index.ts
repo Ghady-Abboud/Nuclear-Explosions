4ece7023d0c41c65956ce362d6da2fef19460030
Ghady Abboud <abboudghady@gmail.com>
2024-11-14 17:59:28 -0500

    Organize Project Structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { Config } from './config/environment.config';
-import routes from './routes/routes';
+import { Config } from '../config/environment.config';
+import routes from '../routes/routes';
 
 const app = express();
 const config = new Config();
@@ -17,7 +17,6 @@ app.use(cors(
 
 
 app.use(express.json());
-
 app.use('/api', routes)
 
 app.listen(PORT, () => {
