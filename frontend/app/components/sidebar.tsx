2356a0b2cd63538a13168ab0a18c78cccf719525
Ghady Abboud <abboudghady@gmail.com>
2024-11-24 22:19:56 -0500

    Refactor Sidebar Component

--- frontend/app/components/sidebar.tsx
+++ frontend/app/components/sidebar.tsx
@@ -2,8 +2,9 @@ import { faCaretDown, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import GetData from "../controllers/fetch";
+import styles from "../styles/sidebar.module.css";
+
 export default function Sidebar() {
-  const [isHovered, setIsHovered] = useState<boolean>(false);
   const bomblist: string[] = ["Mk-I", "Mk-III", "he", "molotov"];
   const [modifiedBombList, setModifiedBombList] = useState<string[]>(bomblist);
   const [dropdownState, setDropdownState] = useState<boolean>(false);
@@ -30,87 +31,27 @@ export default function Sidebar() {
 
   return (
     //Header
-    <div
-      className="side-bar-wrapper"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "20%",
-        overflow: "auto",
-        flexGrow: 1,
-        backgroundColor: "yellow",
-        padding: "10px",
-      }}
-    >
+    <div className={`${styles.sideBarWrapper}`}>
       <h2 style={{ textAlign: "center" }}>NUKEMAP</h2>
-      <hr
-        style={{
-          border: "none",
-          borderTop: "3px solid gray",
-          margin: "10px 0",
-        }}
-      />
+      <hr className={`${styles.sideBarHr}`} />
 
       <button
         //Detonate
-        style={{
-          width: "40%",
-          height: "50px",
-          margin: "0px auto",
-          marginBottom: "20px",
-          borderRadius: "10px",
-          backgroundColor: "red",
-          transform: isHovered ? "scale(1.05)" : "scale(1)",
-          animation: isHovered ? "0.5s ease-in-out" : "none",
-          cursor: "pointer",
-        }}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        className={`${styles.detonateButton}`}
         onClick={() => GetData(searchValue)}
       >
-        <h3
-          style={{
-            textAlign: "center",
-            color: "gray",
-            fontSize: "30px",
-            fontWeight: "bold",
-            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-          }}
-        >
-          Detonate
-        </h3>
+        Detonate
       </button>
 
-      <div
-        className="side-bar-main"
-        style={{
-          position: "relative",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+      <div className={`${styles.sideBarMain}`}>
         <div
           //Select Bomb
-          className="select-bomb"
-          style={{
-            position: "relative",
-            width: "100%",
-            height: dropdownState ? "25%" : "fit-content",
-            display: "flex",
-            flexDirection: "row",
-            marginBottom: "10px",
-          }}
+          className={`${styles.selectBomb} ${
+            dropdownState ? styles.bombDropDownOpen : styles.bombDropDownClosed
+          }`}
         >
           <button
-            style={{
-              width: "50%",
-              height: "40px",
-              borderRadius: "10px",
-              cursor: "pointer",
-              marginRight: "10px",
-            }}
+            className={`${styles.selectBombButton}`}
             onClick={() => setDropdownState(!dropdownState)}
           >
             {searchValue ? searchValue : "Select Bomb"}
@@ -121,52 +62,17 @@ export default function Sidebar() {
           </button>
 
           {dropdownState && (
-            <div
-              className="side-bar-dropdown"
-              style={{
-                width: "100%",
-                height: "100%",
-                position: "relative",
-                overflowY: "auto",
-              }}
-            >
+            <div className={`${styles.sideBarDropDown}`}>
               <input
+                className={`${styles.bombNameInput}`}
                 type="text"
                 placeholder="Search..."
                 value={searchValue}
                 onChange={handleSearchChange}
-                style={{
-                  width: "50%",
-                  height: "30px",
-                  textAlign: "center",
-                  borderRadius: "10px",
-                }}
               />
-              <ul
-                style={{
-                  position: "absolute",
-                  top: "40px",
-                  width: "100%",
-                  backgroundColor: "gray",
-                  borderRadius: "10px",
-                  textAlign: "center",
-                  listStyleType: "none",
-                }}
-              >
+              <ul className={`${styles.bombNameList}`}>
                 {modifiedBombList.map((item, index) => (
-                  <li
-                    onClick={() => handleSelectBomb(item)}
-                    key={index}
-                    style={{
-                      alignContent: "center",
-                      height: "35px",
-                      cursor: "pointer",
-                      borderBottom:
-                        index !== modifiedBombList.length - 1
-                          ? "1px solid white"
-                          : "none",
-                    }}
-                  >
+                  <li onClick={() => handleSelectBomb(item)} key={index}>
                     {item}
                   </li>
                 ))}
@@ -174,100 +80,44 @@ export default function Sidebar() {
             </div>
           )}
         </div>
-        <hr style={{ border: "none", borderTop: "3px solid gray" }} />
+        <hr className={`${styles.sideBarHr}`} />
         <div
           //Basic Options
-          className="basic-options"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            position: "relative",
-            width: "100%",
-            flexGrow: 1,
-          }}
+          className={`${styles.basicOptions}`}
         >
-          <h1 style={{ margin: "5px", fontSize: "16px" }}>Basic Options:</h1>
-          <div
-            className="explosion-type-list"
-            style={{ display: "flex", marginLeft: "30px" }}
-          >
-            <label
-              style={{ margin: "5px", fontStyle: "italic", fontSize: "14px" }}
-            >
+          <h1>Basic Options:</h1>
+          <div className={`${styles.explosionTypeList}`}>
+            <label className={`${styles.explosionTypeListLabel}`}>
               Height of burst:{" "}
             </label>
-            <div
-              className="explosion-type-select"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-evenly",
-                marginLeft: "10px",
-              }}
-            >
-              <div
-                className="airburst"
-                style={{ display: "flex", flexDirection: "row" }}
-              >
+            <div className={`${styles.explosionTypeSelect}`}>
+              <div className={`${styles.airburst}`}>
                 <input type="radio" name="explosion-type" />
-                <label style={{ fontSize: "14px", marginLeft: "5px" }}>
-                  Airburst
-                </label>
+                <label>Airburst</label>
               </div>
-              <div
-                className="surface"
-                style={{ display: "flex", flexDirection: "row" }}
-              >
+              <div className={`${styles.surface}`}>
                 <input type="radio" name="explosion-type" />
-                <label style={{ fontSize: "14px", marginLeft: "5px" }}>
-                  Surface
-                </label>
+                <label>Surface</label>
               </div>
             </div>
           </div>
-          <div
-            className="other-effects"
-            style={{
-              display: "flex",
-              width: "100%",
-              alignItems: "center",
-              marginTop: "10px",
-            }}
-          >
-            <h6
-              style={{
-                marginLeft: "30px",
-                fontSize: "14px",
-                fontStyle: "italic",
-                fontFamily: "sans-serif",
-                color: "darkgray",
-              }}
-            >
-              Other Effects:
-            </h6>
-            <div
-              className="other-effects-list"
-              style={{ display: "flex", alignItems: "center" }}
-            >
-              <input
-                type="checkbox"
-                name="other-effects"
-                style={{ marginLeft: "10px" }}
-              />
-              <label style={{ fontSize: "14px", marginLeft: "5px" }}>
-                Casualties
-              </label>
+          <div className={`${styles.otherEffects}`}>
+            <h6>Other Effects:</h6>
+            <div className={`${styles.otherEffectsList}`}>
+              <input type="checkbox" />
+              <label>Casualties</label>
 
-              <input
-                type="checkbox"
-                name="other-effects"
-                style={{ marginLeft: "10px" }}
-              />
-              <label style={{ fontSize: "14px", marginLeft: "5px" }}>
-                Radioactive fallout
-              </label>
+              <input type="checkbox" />
+              <label>Radioactive fallout</label>
             </div>
           </div>
+        </div>
+        <hr className={`${styles.sideBarHr}`} />
+        <div
+          className="bomb-effect"
+          style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+        >
+          <h1>hello</h1>
         </div>
       </div>
     </div>
